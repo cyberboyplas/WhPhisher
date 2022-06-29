3da6dc09304a0919b4c815aa628b859f712e0962
WhBeatZ <93139343+WhBeatZ@users.noreply.github.com>
2022-06-29 21:34:39 +0200

    Update WhPhisher.py

--- WhPhisher.py
+++ WhPhisher.py
@@ -171,14 +171,9 @@ def update():
         upask=input(ask+"Quieres actualizar WhPhisher (recomendado) pulsa [y] para actualizar o [n] para cancelar --> "+bwhite)
         if upask=="y":
             print(nc)
-            system("rm -rf WhPhisher.py && wget https://raw.githubusercontent.com/WhBeatZ/WhPhisher/main/WhPhisher.py")
-            system("rm -rf NgrokWh")
-            system("git clone https://github.com/WhBeatZ/NgrokWh.git")
-            system("cd NgrokWh")
-            system("chmod +x ngrokwh.sh")
-            system("cd ..")
-            system("chmod +x WhPhisher.py")
             system("cd .. && rm -rf .websites")
+            system("git clone https://github.com/WhBeatZ/WhPhisher")
+            system("chmod +x WhPhisher.py")
             sprint("\n"+success+"WhPhisher se instalo correctamente :)!! Reinicia la terminal :D!\n")
 
             if (changelog != "404: Not Found"):
